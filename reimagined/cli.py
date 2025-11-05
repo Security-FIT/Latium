3508c1fcc3697806f0f69dbc747d67559e43d406
Jakub Res <xresja@gmail.com>
2025-11-05 12:37:23 +0100

    Basic CLI interface added

--- reimagined/cli.py
+++ reimagined/cli.py
@@ -9,9 +9,40 @@
 #
 # Author: Jakub Res iresj@fit.vut.cz
 
+from .utils import print_modules, load_pretrained
+from .rome.causal_trace.causal_trace import causal_trace, compute_multiplier
+import argparse
+import hydra
+from omegaconf import DictConfig
 
-if __name__ == "__main__":
-    def main():
-        raise NotImplementedError
-    
+# Function for  printing the architecture of a certain model and the names of the model's modules
+def print_model_architecture(cfg: DictConfig) -> None:
+    """
+    Print the architecture of a certain model and the names of the model's modules
+    """
+    model, _ = load_pretrained(cfg)
+    print(model)
+    print(model.config)
+    print_modules(model)
+
+@hydra.main(version_base=None, config_path="config", config_name="config")
+def main(cfg: DictConfig) -> None:
+    """
+    Main function for the Reimagined Framework CLI
+    """
+    parser = argparse.ArgumentParser(description="Reimagined Framework CLI")
+    parser.add_argument("+print-arch", help="Print the architecture of the model")
+    parser.add_argument("+causal-trace", help="Run the causal trace task")
+
+    if getattr(cfg, "print-arch", False):
+        print_model_architecture(cfg)
+    elif getattr(cfg, "causal-trace", False):
+        causal_trace(cfg)
+    elif getattr(cfg, "compute-multiplier", False):
+        print(compute_multiplier(cfg))
+    else:
+        parser.print_help()
+        exit(1)
+
+if __name__ == "__main__":    
     main()
